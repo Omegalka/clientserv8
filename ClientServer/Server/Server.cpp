9662ca7622028fe3f39affa8701b9f49d93ac6ac
Omegalka <134694780+Omegalka@users.noreply.github.com>
2024-04-19 15:18:37 +0300

    Add files via upload

---
+++ ClientServer/Server/Server.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <vector>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define MAX_CLIENTS 10
+#define DEFAULT_BUFLEN 512
+
+SOCKET server_socket;
+std::vector<std::string> orders;
+
+int main() {
+    using namespace std;
+
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        cout << "Failed. Error Code: " << WSAGetLastError() << "\n";
+        return 1;
+    }
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        cout << "Could not create socket: " << WSAGetLastError() << "\n";
+        return 2;
+    }
+
+    sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8888);
+
+    if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+        cout << "Bind failed with error code: " << WSAGetLastError() << "\n";
+        return 3;
+    }
+
+    listen(server_socket, MAX_CLIENTS);
+
+    fd_set readfds;
+    SOCKET client_socket[MAX_CLIENTS] = {};
+
+    while (true) {
+        FD_ZERO(&readfds);
+        FD_SET(server_socket, &readfds);
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            SOCKET s = client_socket[i];
+            if (s > 0) {
+                FD_SET(s, &readfds);
+            }
+        }
+
+        if (select(0, &readfds, NULL, NULL, NULL) == SOCKET_ERROR) {
+            cout << "select function call failed with error code : " << WSAGetLastError() << "\n";
+            return 4;
+        }
+
+        SOCKET new_socket;
+        sockaddr_in address;
+        int addrlen = sizeof(sockaddr_in);
+        if (FD_ISSET(server_socket, &readfds)) {
+            if ((new_socket = accept(server_socket, (sockaddr*)&address, &addrlen)) < 0) {
+                cout << "accept function error\n";
+                return 5;
+            }
+
+            printf("New connection, socket fd is %d, ip is: %s, port: %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+            for (int i = 0; i < orders.size(); i++)
+            {
+                cout << "Order #" << i + 1 << ": " << orders[i] << "\n";
+                send(new_socket, orders[i].c_str(), orders[i].size(), 0);
+            }
+
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (client_socket[i] == 0) {
+                    client_socket[i] = new_socket;
+                    printf("Adding to list of sockets at index %d\n", i);
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX_CLIENTS; i++)
+        {
+            SOCKET s = client_socket[i];
+            if (FD_ISSET(s, &readfds))
+            {
+                getpeername(s, (sockaddr*)&address, (int*)&addrlen);
+
+                char client_message[DEFAULT_BUFLEN];
+                int client_message_length = recv(s, client_message, DEFAULT_BUFLEN, 0);
+                client_message[client_message_length] = '\0';
+
+                string check_exit = client_message;
+                if (check_exit == "off")
+                {
+                    cout << "Client #" << i << " is off\n";
+                    client_socket[i] = 0;
+                }
+
+                string temp = client_message;
+                orders.push_back(temp);
+
+                for (int i = 0; i < MAX_CLIENTS; i++) {
+                    if (client_socket[i] != 0) {
+                        send(client_socket[i], client_message, client_message_length, 0);
+                    }
+                }
+            }
+        }
+    }
+
+    WSACleanup();
+    return 0;
+}
+
+
+
+
---
+++ ClientServer/client/client.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <string>
+#include <thread>
+#include <chrono>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 512
+#define SERVER_IP "127.0.0.1"
+#define DEFAULT_PORT "8888"
+
+SOCKET client_socket;
+
+
+
+DWORD WINAPI Sender(void* param)
+{
+    while (true) {
+        string order;
+        cout << "Enter your order: ";
+        getline(cin, order);
+
+        send(client_socket, order.c_str(), order.size(), 0);
+    }
+}
+
+DWORD WINAPI Receiver(void* param)
+{
+    while (true) {
+        char response[DEFAULT_BUFLEN];
+        int result = recv(client_socket, response, DEFAULT_BUFLEN, 0);
+        response[result] = '\0';
+
+        cout << "Received message from server: " << response << "\n";
+    }
+}
+
+int main()
+{
+    using namespace std;
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        cout << "WSAStartup failed with error: " << iResult << "\n";
+        return 1;
+    }
+
+    addrinfo hints = {};
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    addrinfo* result = nullptr;
+    iResult = getaddrinfo(SERVER_IP, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        cout << "getaddrinfo failed with error: " << iResult << "\n";
+        WSACleanup();
+        return 2;
+    }
+
+    addrinfo* ptr = nullptr;
+    for (ptr = result; ptr != nullptr; ptr = ptr->ai_next) {
+        client_socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (client_socket == INVALID_SOCKET) {
+            cout << "socket failed with error: " << WSAGetLastError() << "\n";
+            WSACleanup();
+            return 3;
+        }
+
+        iResult = connect(client_socket, ptr->ai_addr, static_cast<int>(ptr->ai_addrlen));
+        if (iResult == SOCKET_ERROR) {
+            closesocket(client_socket);
+            client_socket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (client_socket == INVALID_SOCKET) {
+        cout << "Unable to connect to server!\n";
+        WSACleanup();
+        return 4;
+    }
+
+    CreateThread(0, 0, Sender, 0, 0, 0);
+    CreateThread(0, 0, Receiver, 0, 0, 0);
+
+    Sleep(INFINITE);
+
+    WSACleanup();
+    return 0;
+}
+
